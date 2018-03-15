a3f0166b0fcf0c34fc14909e8b390d67cfde2144
sexfat <bryant6831@gmail.com>
2018-03-15 18:16:35 +0800

    add

--- dev/js/scroll.js
+++ dev/js/scroll.js
@@ -1,16 +1,23 @@
-scroll(function (direction) {
-    console.log(direction)
-});
+// import { disconnect } from "cluster";
 
-function scroll(fn) {
-    var beforeScrollTop = document.body.scrollTop,
-        fn = fn || function () {};
-    window.addEventListener("scroll", function () {
-        
-        var afterScrollTop = document.body.scrollTop,
-            delta = afterScrollTop - beforeScrollTop;
-            if (delta === 0) return false;
-         fn(delta > 0 ? "down" : "up");
-         beforeScrollTop = afterScrollTop;
-    }, false);
+//滾動
+var last_known_scroll_position = 0;
+console.log(last_known_scroll_position);
+
+window.addEventListener("scroll", function(){
+  //取得最後位址
+   last_known_scroll_position = window.scrollY;
+    console.log(last_known_scroll_position);
+ });
+
+
+var names = ["obj1" , "obj2" , "obj3" , "obj4"];
+var text = "";
+var i;
+for (i = 0 ; i< names.length; i++) {
+      text += "<li>" + names[i] + "</li>" + "<br/>";
 }
+document.getElementById("nav").innerHTML = text;
+console.log("ok");
+
+
